217e2f9b54bb8399b6ccc9225eb04a52d6166176
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-04-28 14:07:28 +0530

    added Compair Line

--- LineCamparision/LineLengthCalculator.cs
+++ LineCamparision/LineLengthCalculator.cs
@@ -10,31 +10,38 @@ namespace LineCamparision
     {
         public static void LineLengthcalculator()
         {
-            double x1, y1, x2, y2;
+            Console.WriteLine("Enter the (x,y) coordinates of the first point of line 1:");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the (x,y) coordinates of the first point:");
-            x1 = double.Parse(Console.ReadLine());
-            y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the (x,y) coordinates of the second point of line 1:");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter the (x,y) coordinates of the second point:");
-            x2 = double.Parse(Console.ReadLine());
-            y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the (x,y) coordinates of the first point of line 2:");
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
 
-            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Enter the (x,y) coordinates of the second point of line 2:");
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine($"The length of the line connecting ({x1},{y1}) and ({x2},{y2}) is {length}");
-            LineAndLengthCalculator.LineEqualityChecker( x1,  y1, x2,y2);
-        }
-        public static void LineEqualityChecker(double x1 , double y1 , double x2, double y2)
-        {
-            
-            if ((x1 == y1 && y2 == x2 ) || (x1 == x2 && y1 == y2 ))
+            double length1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double length2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
+
+            int comparisonResult = length1.CompareTo(length2);
+
+            if (comparisonResult == 0)
             {
-                Console.WriteLine("The two lines are equal.");
+                Console.WriteLine("Line 1 is equal to line 2.");
+            }
+            else if (comparisonResult < 0)
+            {
+                Console.WriteLine("Line 1 is less than line 2.");
             }
             else
             {
-                Console.WriteLine("The two lines are not equal.");
+                Console.WriteLine("Line 1 is greater than line 2.");
             }
         }
     }
