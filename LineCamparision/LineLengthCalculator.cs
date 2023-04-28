42f029dfc0c13fcdb5ceedd9c155388775350f6e
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-04-28 14:01:46 +0530

    added Checked Line Equl Or Not

--- LineCamparision/LineLengthCalculator.cs
+++ LineCamparision/LineLengthCalculator.cs
@@ -8,21 +8,34 @@ namespace LineCamparision
 {
     public class LineAndLengthCalculator
     {
-        public static void LineLengthcalculator()    
+        public static void LineLengthcalculator()
         {
-                double x1, y1, x2, y2;
+            double x1, y1, x2, y2;
 
-                Console.WriteLine("Enter the (x,y) coordinates of the first point:");
-                x1 = double.Parse(Console.ReadLine());
-                y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the (x,y) coordinates of the first point:");
+            x1 = double.Parse(Console.ReadLine());
+            y1 = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Enter the (x,y) coordinates of the second point:");
-                x2 = double.Parse(Console.ReadLine());
-                y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the (x,y) coordinates of the second point:");
+            x2 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
 
-                double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 
-                Console.WriteLine($"The length of the line connecting ({x1},{y1}) and ({x2},{y2}) is {length}");
+            Console.WriteLine($"The length of the line connecting ({x1},{y1}) and ({x2},{y2}) is {length}");
+            LineAndLengthCalculator.LineEqualityChecker( x1,  y1, x2,y2);
+        }
+        public static void LineEqualityChecker(double x1 , double y1 , double x2, double y2)
+        {
+            
+            if ((x1 == y1 && y2 == x2 ) || (x1 == x2 && y1 == y2 ))
+            {
+                Console.WriteLine("The two lines are equal.");
             }
+            else
+            {
+                Console.WriteLine("The two lines are not equal.");
+            }
+        }
     }
 }
